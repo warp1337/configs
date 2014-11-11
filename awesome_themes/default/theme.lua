9a31d0dc535273f22634144de98eb0d0c10236b1
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-11 17:56:48 +0100

    removed nice and clean

--- awesome_themes/default/theme.lua
+++ awesome_themes/default/theme.lua
@@ -76,6 +76,10 @@ theme.titlebar_maximized_button_focus_active  = theme_path .. "/titlebar/maximiz
 
 theme.wallpaper = theme_path .. "/aurora-desktop.png"
 
+-- Conky
+wpscript      = theme_path .. "/niceandclean.sh"
+theme.wallpaper_cmd = { "sh " .. wpscript .. " " .. 1920 }
+
 -- You can use your own layout icons like this:
 theme.layout_fairh = theme_path .. "/layouts/fairhw.png"
 theme.layout_fairv = theme_path .. "/layouts/fairvw.png"
